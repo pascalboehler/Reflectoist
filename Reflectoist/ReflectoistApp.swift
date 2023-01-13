04ee583420b89edcc3f74a46d5bf1258d4e519ef
Pascal Böhler <pascal.boehler@hotmail.de>
2023-01-13 15:55:04 +0100

    Initial Commit

---
+++ Reflectoist/ReflectoistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflectoistApp.swift
+//  Reflectoist
+//
+//  Created by Pascal Boehler on 13.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ReflectoistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
